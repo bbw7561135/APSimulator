dc9ec25eb3b8cd2a42e3183b95705a8e7eccd5c1
Lars Frogner <lars_frogner@hotmail.com>
2018-12-23 18:03:45 +0100

    Move method for constructing array out of FilterSet class

--- filters.py
+++ filters.py
@@ -4,6 +4,22 @@ from builtins import zip
 import numpy as np
 
 
+def construct_filtered_flux_array(filtered_fluxes, filter_names, filter_indices, color_axis=0):
+
+    n_included_filters = len(filter_names)
+
+    assert len(filter_indices) == n_included_filters
+    assert set(filter_indices) == set(range(n_included_filters))
+    assert set(filter_names).issubset(filtered_fluxes.keys())
+
+    filtered_flux_array = np.zeros((len(filter_names), *filtered_fluxes[filter_names[0]].shape), dtype=filtered_fluxes[filter_names[0]].dtype)
+
+    for idx in range(n_included_filters):
+        filtered_flux_array[filter_indices[idx], :] = filtered_fluxes[filter_names[idx]]
+
+    return filtered_flux_array.swapaxes(0, color_axis)
+
+
 class FilterSet:
     '''
     Class for holding a set of filters and computing fluxes integrated through
@@ -21,21 +37,6 @@ class FilterSet:
             filtered_fluxes[filter_name] = self.filters[filter_name].compute_integrated_flux(wavelengths, spectral_fluxes, wavelength_axis)
 
         return filtered_fluxes
-
-    def construct_filtered_flux_array(self, filtered_fluxes, filter_names, filter_indices, color_axis=0):
-
-        n_included_filters = len(filter_names)
-
-        assert len(filter_indices) == n_included_filters
-        assert set(filter_indices) == set(range(n_included_filters))
-        assert set(filter_names).issubset(filtered_fluxes.keys())
-
-        filtered_flux_array = np.zeros((len(filter_names), *filtered_fluxes[filter_names[0]].shape), dtype=filtered_fluxes[filter_names[0]].dtype)
-
-        for idx in range(n_included_filters):
-            filtered_flux_array[filter_indices[idx], :] = filtered_fluxes[filter_names[idx]]#/(self.filters[filter_names[idx]].maximum_wavelength - self.filters[filter_names[idx]].minimum_wavelength)
-
-        return filtered_flux_array.swapaxes(0, color_axis)
 
     def has(self, filter_names):
         return set(filter_names).issubset(self.filters.keys())
