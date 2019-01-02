bc25817cea0b8bbaa03477437a2faa5c19eb169a
Lars Frogner <lars_frogner@hotmail.com>
2019-01-02 12:36:14 +0100

    Update to use new field processing system and add various minor features and improvements

--- apsimulator/imaging_system.py
+++ apsimulator/imaging_system.py
@@ -1,24 +1,29 @@
 # -*- coding: utf-8 -*-
+# This file is part of the APSimulator API.
+# Author: Lars Frogner
 import numpy as np
+import matplotlib.pyplot as plt
 import math_utils
 import grids
 import fields
-import field_combination
+import field_processing
 
 
 class ImagingSystem:
+    '''
+    This class organizes the image formation pipeline. It is responsible for holding all
+    the objects used at various stages of the image generation process and communicating
+    intermediate results between them.
+    '''
+    def __init__(self, field_of_view_x, field_of_view_y, angular_coarseness, wavelengths, use_memmaps=False):
+        self.field_of_view_x = float(field_of_view_x) # Field of view in the x-direction [rad]
+        self.field_of_view_y = float(field_of_view_y) # Field of view in the y-direction [rad]
+        self.angular_coarseness = float(angular_coarseness) # Angle subtended by a pixel in the center of the image plane [rad]
+        self.wavelengths = np.asfarray(wavelengths) # Array of wavelengths for the incident light [m]
+        self.use_memmaps = bool(use_memmaps) # Whether to conserve memory at the cost of performance by storing fields in memory mapped files
 
-    def __init__(self, field_of_view_x, field_of_view_y, angular_coarseness, wavelengths):
-        self.field_of_view_x = float(field_of_view_x)
-        self.field_of_view_y = float(field_of_view_y)
-        self.angular_coarseness = float(angular_coarseness)
-
-        self.wavelengths = np.asfarray(wavelengths)
         self.n_wavelengths = len(self.wavelengths)
         assert(self.wavelengths.ndim == 1)
-
-        self.source_field_combiner = field_combination.FieldCombiner(initial_field_value=0, dtype='float64')
-        self.aperture_modulation_combiner = field_combination.FieldCombiner(initial_field_value=1, dtype='complex128')
 
         self.initialize_fields()
 
@@ -39,9 +44,12 @@ class ImagingSystem:
         self.add_aperture_modulator('aperture', aperture)
         self.has_aperture = True
 
-    def initialize_source_field(self, use_memmap=False):
+    def initialize_source_field(self):
         '''
         Constructs the grid for the source field and initializes the field.
+
+        The source field is the angular and spectral distribution of amplitudes
+        of the light field incident on the aperture, and has units of sqrt(W/m^2/m).
 
         The source field grid is defined in terms of the x- and y-components of
         the unit direction vector from the center of the aperture to the source.
@@ -66,13 +74,22 @@ class ImagingSystem:
         self.source_grid.define_window((-half_field_of_view_extent_x, half_field_of_view_extent_x),
                                        (-half_field_of_view_extent_y, half_field_of_view_extent_y))
 
-        # Initialize the object holding the sources
-        self.source_field_combiner.initialize_combined_field(self.source_grid, self.wavelengths,
-                                                             use_memmap=use_memmap)
+        source_field = fields.SpectralField(self.source_grid, self.wavelengths,
+                                            dtype='float64',
+                                            initial_value=0,
+                                            use_memmap=self.use_memmaps)
 
-    def initialize_aperture_field(self, use_memmap=False):
+        # Initialize the pipeline object for adding source fluxes to the source field
+        self.source_pipeline = field_processing.FieldProcessingPipeline(source_field)
+
+    def initialize_aperture_field(self):
         '''
         Constructs the grid for the aperture field and initializes the field.
+
+        The aperture field is the spatial and spectral distribution of the total light
+        field incident on the aperture from all sources. The field values are complex,
+        representing both field strength (in the form of amplitudes) and field phases.
+        The field values have units of sqrt(W/m^2/m).
 
         The aperture field grid is defined in terms of the spatial x- and y-coordinates
         with respect to the aperture center in the plane of the aperture. However, the
@@ -85,11 +102,10 @@ class ImagingSystem:
         self.aperture_field = fields.SpectralField(self.aperture_grid, self.wavelengths,
                                                    initial_value=0,
                                                    dtype='complex128',
-                                                   use_memmap=use_memmap)
+                                                   use_memmap=self.use_memmaps)
 
-        # Initialize the object holding the aperture field modulations
-        self.aperture_modulation_combiner.initialize_combined_field(self.aperture_grid, self.wavelengths,
-                                                                    use_memmap=use_memmap)
+        # Initialize the pipeline object for modulating the aperture field
+        self.aperture_modulation_pipeline = field_processing.FieldProcessingPipeline(self.aperture_field)
 
     def initialize_aperture_grid_window(self):
         assert self.has_imager
@@ -98,26 +114,34 @@ class ImagingSystem:
         self.aperture_grid.define_window((-max_normalized_aperture_radius, max_normalized_aperture_radius),
                                          (-max_normalized_aperture_radius, max_normalized_aperture_radius))
 
-    def initialize_image_field(self, use_memmap=False):
+    def initialize_image_field(self):
         assert self.has_imager
+
         self.imager.initialize_image_field(self.aperture_grid, self.wavelengths,
                                            self.field_of_view_x, self.field_of_view_y,
-                                           use_memmap=use_memmap)
+                                           use_memmap=self.use_memmaps)
 
-    def add_source(self, name, source, keep_field=False):
-        self.source_field_combiner.add(name, source, keep_field=keep_field)
+        self.image_postprocessing_pipeline = field_processing.FieldProcessingPipeline(self.imager.get_image_field())
 
-    def add_aperture_modulator(self, name, aperture_modulator, keep_field=False):
-        self.aperture_modulation_combiner.add(name, aperture_modulator, keep_field=keep_field)
+    def add_source(self, name, source, store_field=False):
+        self.source_pipeline.add_field_processor(name, source, store_process_field_if_possible=store_field)
+
+    def add_aperture_modulator(self, name, aperture_modulator, store_field=False):
+        self.aperture_modulation_pipeline.add_field_processor(name, aperture_modulator, store_process_field_if_possible=store_field)
+
+    def add_image_postprocessor(self, name, image_postprocessor):
+        assert self.has_imager
+        self.image_postprocessing_pipeline.add_field_processor(name, image_postprocessor)
 
     def run_full_propagation(self):
         self.compute_source_field()
         self.compute_aperture_field()
         self.compute_aperture_modulation()
         self.compute_image_field()
+        self.compute_postprocessed_image_field()
 
     def compute_source_field(self):
-        self.source_field_combiner.compute_combined_field()
+        self.source_pipeline.compute_processed_field()
 
     def compute_aperture_field(self):
         '''
@@ -128,26 +152,37 @@ class ImagingSystem:
         oscillations of the electromagnetic field are neglected (since these will average
         out over a very short time).
         '''
-        combined_source_field = self.source_field_combiner.get_combined_field()
-        self.aperture_field.set_values_inside_window(combined_source_field.fourier_transformed_values())
+        total_source_field = self.source_pipeline.get_processed_field()
+        self.aperture_field.set_values_inside_window(total_source_field.fourier_transformed_values())
 
     def compute_aperture_modulation(self):
-        self.aperture_modulation_combiner.compute_combined_field()
+        self.aperture_modulation_pipeline.compute_processed_field()
+
+    def compute_transmitted_aperture_field(self):
+        assert self.has_aperture
+        aperture = self.aperture_modulation_pipeline.get_processor('aperture')
+        transmitted_aperture_field = self.aperture_field.copy()
+        aperture.process(transmitted_aperture_field)
+        return transmitted_aperture_field
 
     def compute_modulated_aperture_field(self):
-        combined_modulation_field = self.aperture_modulation_combiner.get_combined_field()
-        modulated_aperture_field = self.aperture_field.multiplied_within_window(combined_modulation_field.values)
+        total_modulation_field = self.aperture_modulation_pipeline.get_processed_field()
+        modulated_aperture_field = self.aperture_field.multiplied_within_window(total_modulation_field.values)
         return modulated_aperture_field
 
     def compute_image_field(self):
         assert self.has_imager
         self.imager.compute_image_field(self.compute_modulated_aperture_field())
 
+    def compute_postprocessed_image_field(self):
+        assert self.has_imager
+        self.image_postprocessing_pipeline.compute_processed_field()
+
     def get_source(self, name):
-        return self.source_field_combiner.get(name)
+        return self.source_pipeline.get(name)
 
     def get_aperture_modulator(self, name):
-        return self.aperture_modulation_combiner.get(name)
+        return self.aperture_modulation_pipeline.get(name)
 
     def get_imager(self):
         assert self.has_imager
@@ -155,22 +190,59 @@ class ImagingSystem:
 
     def get_aperture(self):
         assert self.has_aperture
-        return self.aperture_modulation_combiner.get('aperture')
+        return self.aperture_modulation_pipeline.get('aperture')
+
+    def compute_rayleigh_limit(self, wavelength):
+        assert self.has_imager
+        return 1.22*wavelength/self.imager.get_aperture_diameter()
+
+    def compute_spectral_powers_of_aperture_field(self, aperture_field):
+        return np.sum(math_utils.abs2(aperture_field.get_values_inside_window()), axis=(1, 2))*self.wavelengths**2*self.aperture_grid.compute_cell_area()
+
+    def compute_incident_spectral_powers(self):
+        return self.compute_spectral_powers_of_aperture_field(self.compute_transmitted_aperture_field())
+
+    def compute_modulated_spectral_powers(self):
+        return self.compute_spectral_powers_of_aperture_field(self.compute_modulated_aperture_field())
+
+    def visualize_energy_conservation(self):
+        assert self.has_imager
+        incident_spectral_powers = self.compute_incident_spectral_powers()
+        modulated_spectral_powers = self.compute_modulated_spectral_powers()
+        image_spectral_powers = self.imager.compute_spectral_powers()
+        postprocessed_image_spectral_powers = self.imager.compute_spectral_powers_of_image_field(self.image_postprocessing_pipeline.get_processed_field())
+
+        incident_power = np.trapz(incident_spectral_powers, x=self.wavelengths)
+        modulated_power = np.trapz(modulated_spectral_powers, x=self.wavelengths)
+        image_power = np.trapz(image_spectral_powers, x=self.wavelengths)
+        postprocessed_image_power = np.trapz(postprocessed_image_spectral_powers, x=self.wavelengths)
+
+        fig, ax = plt.subplots()
+        alpha = 1
+        ax.plot(self.wavelengths, incident_spectral_powers, '-', alpha=alpha, label='Incident ({:g} W total)'.format(incident_power))
+        ax.plot(self.wavelengths, modulated_spectral_powers, '--', alpha=alpha, label='Modulated ({:g} W total)'.format(modulated_power))
+        ax.plot(self.wavelengths, image_spectral_powers, ':', alpha=alpha, label='Image ({:g} W total)'.format(image_power))
+        ax.plot(self.wavelengths, postprocessed_image_spectral_powers, '-.', alpha=alpha, label='Postprocessed image ({:g} W total)'.format(postprocessed_image_power))
+        ax.set_title('Energy conservation')
+        ax.set_xlabel('Wavelength [m]')
+        ax.set_ylabel('Spectral power [W/m]')
+        ax.legend(loc='best')
+        plt.show()
 
     def visualize_source_field(self, name, **plot_kwargs):
-        self.source_field_combiner.visualize(name, **plot_kwargs)
+        self.source_pipeline.visualize_process_field(name, **plot_kwargs)
 
     def visualize_aperture_modulation_field(self, name, **plot_kwargs):
-        self.aperture_modulation_combiner.visualize(name, **plot_kwargs)
+        self.aperture_modulation_pipeline.visualize_process_field(name, **plot_kwargs)
 
-    def visualize_combined_source_field(self, **plot_kwargs):
-        fields.visualize_field(self.source_field_combiner.get_combined_field(), **plot_kwargs)
+    def visualize_total_source_field(self, **plot_kwargs):
+        fields.visualize_field(self.source_pipeline.get_processed_field(), **plot_kwargs)
 
     def visualize_aperture_field(self, **plot_kwargs):
         fields.visualize_field(self.aperture_field, **plot_kwargs)
 
-    def visualize_combined_aperture_modulation_field(self, **plot_kwargs):
-        fields.visualize_field(self.aperture_modulation_combiner.get_combined_field(), **plot_kwargs)
+    def visualize_total_aperture_modulation_field(self, **plot_kwargs):
+        fields.visualize_field(self.aperture_modulation_pipeline.get_processed_field(), **plot_kwargs)
 
     def visualize_modulated_aperture_field(self, **plot_kwargs):
         fields.visualize_field(self.compute_modulated_aperture_field(), **plot_kwargs)
@@ -178,3 +250,7 @@ class ImagingSystem:
     def visualize_image_field(self, **plot_kwargs):
         assert self.has_imager
         self.imager.visualize(**plot_kwargs)
+
+    def visualize_postprocessed_image_field(self, **plot_kwargs):
+        assert self.has_imager
+        fields.visualize_field(self.image_postprocessing_pipeline.get_processed_field(), **plot_kwargs)
