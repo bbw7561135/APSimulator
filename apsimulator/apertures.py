fa528b9c5c94cc68f44b6daa92a16ee3f1095736
Lars Frogner <lars_frogner@hotmail.com>
2019-01-02 12:28:57 +0100

    Update to use new field processing system

--- apsimulator/apertures.py
+++ apsimulator/apertures.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
+# This file is part of the APSimulator API.
+# Author: Lars Frogner
 import numpy as np
 import fields
-import field_combination
+import field_processing
 
 
-class CircularAperture(field_combination.FieldGenerator):
+class CircularAperture(field_processing.MultiplicativeFieldProcessor):
 
     def __init__(self, diameter, inner_diameter=0, spider_wane_width=0):
-        self.set_diameter(diameter)
-        self.set_inner_diameter(inner_diameter)
-        self.set_spider_wane_width(spider_wane_width)
+        self.set_diameter(diameter) # Diameter of the aperture [m]
+        self.set_inner_diameter(inner_diameter) # Diameter of the circular central obstruction [m]
+        self.set_spider_wane_width(spider_wane_width) # Width of the horizontal and vertical spider wanes [m]
 
     def set_diameter(self, diameter):
         self.diameter = float(diameter)
@@ -24,10 +26,13 @@ class CircularAperture(field_combination.FieldGenerator):
         self.spider_wane_halfwidth = self.spider_wane_width/2
 
     def compute_blocking_mask(self):
-
+        '''
+        Constructs a modulation field representing the shape of the aperture.
+        '''
         normalized_x_coordinates, normalized_y_coordinates = self.grid.get_coordinate_meshes_within_window()
         squared_normalized_distances = self.grid.compute_squared_distances_within_window()
 
+        # Construct grids in spatial units by multiplying with wavelength
         abs_x_coordinates = np.multiply.outer(self.wavelengths, np.abs(normalized_x_coordinates))
         abs_y_coordinates = np.multiply.outer(self.wavelengths, np.abs(normalized_y_coordinates))
         squared_distances = np.multiply.outer(self.wavelengths**2, squared_normalized_distances)
@@ -39,17 +44,9 @@ class CircularAperture(field_combination.FieldGenerator):
 
         return blocking_mask
 
-    def apply(self, combined_field):
-        combined_field.multiply_within_window(self.compute_blocking_mask())
-
-    def generate_field(self, use_memmap=False):
-
-        self.generated_field = fields.SpectralField(self.grid, self.wavelengths,
-                                                    initial_value=False,
-                                                    dtype='bool',
-                                                    use_memmap=use_memmap)
-
-        self.generated_field.set_values_inside_window(self.compute_blocking_mask())
-
-    def apply_generated_field(self, combined_field):
-        combined_field.multiply_within_window(self.generated_field.values)
+    def process(self, field):
+        '''
+        Implements the FieldProcessor method for modulating the given field with the
+        aperture shape.
+        '''
+        field.multiply_within_window(self.compute_blocking_mask())
