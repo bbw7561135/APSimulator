ee057ca8b853224a7e7832331440f5855cc1515d
Lars Frogner <lars_frogner@hotmail.com>
2019-01-03 23:46:03 +0100

    Add physically correct star field generation

--- apsimulator/sources.py
+++ apsimulator/sources.py
@@ -1,41 +1,92 @@
 # -*- coding: utf-8 -*-
+# This file is part of the APSimulator API.
+# Author: Lars Frogner
 import numpy as np
 import fields
-import field_combination
+import field_processing
 import math_utils
+import physics_utils
 
 
-class StarField(field_combination.FieldGenerator):
+class UniformStarField(field_processing.AdditiveFieldProcessor):
 
-    def __init__(self, star_density, seed=None):
-        self.star_density = float(star_density) # Average number of stars per solid angle
+    def __init__(self, stellar_density, near_distance, far_distance, seed=None):
+        self.stellar_density = float(stellar_density) # Average number of stars per volume [1/m^3]
+        self.near_distance = float(near_distance) # Distance to where the uniform star field begins [m]
+        self.far_distance = float(far_distance) # Distance to where the uniform star field ends [m]
         self.random_generator = np.random.RandomState(seed=seed)
+
+        self.near_distance_cubed = self.near_distance**3
+        self.far_distance_cubed = self.far_distance**3
+
+    def compute_visible_star_field_volume(self, field_of_view_x, field_of_view_y):
+        return (3/4)*np.tan(field_of_view_x/2)*np.tan(field_of_view_y/2)*(self.far_distance_cubed - self.near_distance_cubed)
+
+    def compute_average_number_of_visible_stars(self, field_of_view_x, field_of_view_y):
+        return self.stellar_density*self.compute_visible_star_field_volume(field_of_view_x, field_of_view_y)
+
+    def combine_duplicate_indices(self, indices, values):
+        assert indices.ndim == 1
+        assert values.ndim == 2
+        assert values.shape[1] == indices.size
+
+        unique_indices = np.unique(indices)
+        summed_values = np.empty((values.shape[0], unique_indices.size))
+
+        for i, index in enumerate(unique_indices):
+            summed_values[:, i] = np.sum(values[:, indices == index], axis=1)
+
+        return unique_indices, summed_values
+
+    def generate_distances(self, n_stars):
+        '''
+        Samples distances to the stars using that the probability of finding
+        a distance r is proportional to r^2.
+        '''
+        random_fractions = self.random_generator.random_sample(size=n_stars)
+        return np.cbrt((self.far_distance_cubed - self.near_distance_cubed)*random_fractions + self.near_distance_cubed)
+
+    def generate_temperatures(self, n_stars):
+        return np.full(n_stars, 6000)
+
+    def generate_luminosities(self, n_stars):
+        return np.full(n_stars, physics_utils.solar_luminosity)
 
     def generate_star_field(self):
 
-        field_of_view_x = math_utils.polar_angle_from_direction_vector_extent(self.grid.extent_x)
-        field_of_view_y = math_utils.polar_angle_from_direction_vector_extent(self.grid.extent_y)
-        field_of_view_area = field_of_view_x*field_of_view_y
-        n_stars = int(self.star_density*field_of_view_area)
+        total_number_of_grid_cells = self.grid.get_total_window_size()
+        extent_x, extent_y = self.grid.get_window_extents()
 
-        total_window_size = self.grid.compute_total_window_size()
-        star_indices = self.random_generator.randint(low=0, high=total_window_size, size=n_stars)
-        star_field = np.zeros((self.n_wavelengths, total_window_size), dtype='float64')
-        star_field[:, star_indices] = 1
+        field_of_view_x = math_utils.polar_angle_from_direction_vector_extent(extent_x)
+        field_of_view_y = math_utils.polar_angle_from_direction_vector_extent(extent_y)
 
-        return star_field.reshape((self.n_wavelengths, *self.grid.window.shape))
+        average_number_of_visible_stars = self.compute_average_number_of_visible_stars(field_of_view_x, field_of_view_y)
 
-    def apply(self, combined_field):
-        combined_field.add_within_window(self.generate_star_field())
+        # Draw number of stars to generate from a Poisson distribution
+        n_stars = self.random_generator.poisson(lam=average_number_of_visible_stars)
 
-    def generate_field(self, use_memmap=False):
+        # Generate 1D index into the image array for each star, using a uniform distribution
+        star_indices = self.random_generator.randint(low=0, high=total_number_of_grid_cells, size=n_stars)
 
-        self.generated_field = fields.SpectralField(self.grid, self.wavelengths,
-                                                    initial_value=0,
-                                                    dtype='float64',
-                                                    use_memmap=use_memmap)
+        distances = self.generate_distances(n_stars)
+        temperatures = self.generate_temperatures(n_stars)
+        luminosities = self.generate_luminosities(n_stars)
 
-        self.generated_field.set_values_inside_window(self.generate_star_field())
+        # Compute flux spectrum recieved from each star
+        spectral_fluxes = physics_utils.BlackbodyStars(self.wavelengths, distances, temperatures, luminosities).compute_recieved_spectral_fluxes()
 
-    def apply_generated_field(self, combined_field):
-        combined_field.add_within_window(self.generated_field.values)
+        # Handle multiple stars in the same grid cells by summing the spectral fluxes at with the same indices
+        star_indices, spectral_fluxes = self.combine_duplicate_indices(star_indices, spectral_fluxes)
+
+        star_field = np.zeros((self.n_wavelengths, total_number_of_grid_cells), dtype=self.dtype)
+        star_field[:, star_indices] = spectral_fluxes
+        star_field = star_field.reshape((self.n_wavelengths, *self.grid.window.shape))
+
+        return star_field
+
+    def process(self, field):
+        '''
+        Implements the FieldProcessor method for adding the star field source field
+        to the given field.
+        '''
+        field.add_within_window(self.generate_star_field())
