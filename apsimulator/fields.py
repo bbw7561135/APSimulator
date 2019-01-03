f1a5c573eee44d4544417e9dbf868af55f4a1a1e
Lars Frogner <lars_frogner@hotmail.com>
2019-01-03 23:42:03 +0100

    Rename methods and add autostretch option to plotting function

--- apsimulator/fields.py
+++ apsimulator/fields.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import tempfile
 import grids
 import plot_utils
+import image_utils
 
 
 class Regular2DField:
@@ -52,13 +53,13 @@ class Regular2DField:
                 # Initialize value array with constant number
                 self.values = np.full(self.shape, initial_value, dtype=self.dtype)
 
-    def window_view(self, values):
+    def get_window_view_of_array(self, values):
         assert values.shape == self.shape
         window = self.grid.window
         return values[window.y.start:window.y.end, window.x.start:window.x.end]
 
     def get_values_inside_window(self):
-        return self.window_view(self.values)
+        return self.get_window_view_of_array(self.values)
 
     def set_constant_value(self, constant_value):
         self.values[:] = float(constant_value)
@@ -103,7 +104,7 @@ class Regular2DField:
         window_values = self.get_values_inside_window()
 
         if values.shape == self.shape:
-            window_values[:] = self.window_view(values)
+            window_values[:] = self.get_window_view_of_array(values)
         else:
             window_values[:] = values
 
@@ -119,7 +120,7 @@ class Regular2DField:
         window_values = self.get_values_inside_window()
 
         if factors.shape == self.shape:
-            window_values[:] *= self.window_view(factors)
+            window_values[:] *= self.get_window_view_of_array(factors)
         else:
             window_values[:] *= factors
 
@@ -135,7 +136,7 @@ class Regular2DField:
         window_values = self.get_values_inside_window()
 
         if offsets.shape == self.shape:
-            window_values[:] += self.window_view(offsets)
+            window_values[:] += self.get_window_view_of_array(offsets)
         else:
             window_values[:] += offsets
 
@@ -171,7 +172,7 @@ class Regular2DField:
         offset_field.add_within_window(offsets)
         return offset_field
 
-    def fourier_transformed_values(self, inverse=False):
+    def compute_fourier_transformed_values(self, inverse=False):
         '''
         Computes the 2D Fourier transform of the field values and returns the result as an array.
         If inverse=True, the inverse inverse Fourier transform is computed instead.
@@ -189,8 +190,8 @@ class Regular2DField:
 
         return recentered_fourier_coefficients
 
-    def fourier_transformed_values_inside_window(self, inverse=False):
-        return self.window_view(self.fourier_transformed_values(inverse=inverse))
+    def compute_fourier_transformed_window_values(self, inverse=False):
+        return self.get_window_view_of_array(self.compute_fourier_transformed_values(inverse=inverse))
 
     def to_fourier_space(self, inverse=False, transform_grid=True, use_memmap=None):
         '''
@@ -199,7 +200,7 @@ class Regular2DField:
         '''
         assert isinstance(self.grid, grids.FFTGrid)
 
-        fourier_coefficients = self.fourier_transformed_values(inverse=inverse)
+        fourier_coefficients = self.compute_fourier_transformed_values(inverse=inverse)
 
         # Construct grid for the Fourier transformed field
         grid = self.grid.to_spatial_frequency_grid() if transform_grid else self.grid
@@ -209,12 +210,12 @@ class Regular2DField:
                               use_memmap=(self.use_memmap if use_memmap is None else use_memmap),
                               copy_initial_array=False)
 
-    def construct_window_field(self, grid_type=None, use_memmap=None, copy_values=True):
+    def create_window_field(self, grid_type=None, use_memmap=None, copy_values=True):
         '''
         Returns a new field corresponding to the part of the current field inside
         the grid window.
         '''
-        window_grid = self.grid.construct_window_grid(grid_type=grid_type)
+        window_grid = self.grid.create_window_grid(grid_type=grid_type)
         return self.__class__(*self.create_constructor_argument_list(grid=window_grid),
                               initial_value=self.get_values_inside_window(),
                               use_memmap=(self.use_memmap if use_memmap is None else use_memmap),
@@ -228,8 +229,8 @@ class SpectralField(Regular2DField):
     def __init__(self, grid, wavelengths, **field_kwargs):
         self.wavelengths = np.asfarray(wavelengths) # Array of wavelengths
 
-        self.n_wavelengths = len(self.wavelengths)
         assert(self.wavelengths.ndim == 1)
+        self.n_wavelengths = self.wavelengths.size
 
         super().__init__(grid, **field_kwargs)
 
@@ -244,7 +245,7 @@ class SpectralField(Regular2DField):
         self.shape = (self.n_wavelengths, *self.grid.shape)
         self.window_shape = (self.n_wavelengths, *self.grid.window.shape)
 
-    def window_view(self, values):
+    def get_window_view_of_array(self, values):
         assert values.shape == self.shape
         window = self.grid.window
         return values[:, window.y.start:window.y.end, window.x.start:window.x.end]
@@ -261,7 +262,7 @@ class FilteredSpectralField(SpectralField):
         self.n_channels = self.n_wavelengths
 
         self.filter_labels = list(filter_labels)
-        assert len(self.filter_labels) == len(self.central_wavelengths)
+        assert len(self.filter_labels) == self.central_wavelengths.size
 
         self.channels = {label: idx for label, idx in zip(self.filter_labels, range(self.n_channels))}
 
@@ -270,7 +271,7 @@ class FilteredSpectralField(SpectralField):
                self.wavelengths if not 'wavelengths' in new_args else new_args['wavelengths'], \
                self.filter_labels if not 'filter_labels' in new_args else new_args['filter_labels']
 
-    def construct_field_for_channel(self, filter_label, use_memmap=None, copy_values=True):
+    def create_field_for_channel(self, filter_label, use_memmap=None, copy_values=True):
         assert filter_label in self.channels
         return Regular2DField(self.grid,
                               initial_value=self.values[self.channels[filter_label], :, :],
@@ -278,7 +279,15 @@ class FilteredSpectralField(SpectralField):
                               copy_initial_array=copy_values)
 
 
-def visualize_field(field, only_window=True, approximate_wavelength=None, filter_label=None, clipping_factor=1, use_log=False, title='', output_path=None):
+def visualize_field(field, only_window=True, approximate_wavelength=None, filter_label=None, use_autostretch=False, white_point_scale=1, use_log=False, title='', output_path=None):
+
+    use_colors = False
+    was_stretched = False
+
+    wavelength = None
+
+    vmin = None
+    vmax = None
 
     if only_window:
         field_values = field.get_values_inside_window()
@@ -287,21 +296,14 @@ def visualize_field(field, only_window=True, approximate_wavelength=None, filter
         field_values = field.values
         extent = field.grid.get_bounds()
 
-    use_colors = False
-    wavelength = None
-
-    vmin = None
-    vmax = None
-
     if isinstance(field, FilteredSpectralField):
         if filter_label is None:
             use_colors = True
-            vmin = np.min(field_values)
-            vmax = np.max(field_values)*clipping_factor
-            field_values = (field_values - vmin)/(vmax - vmin)
-            field_values = np.moveaxis(field_values, 0, 2)
-            clipping_factor = None
             use_log = False
+            field_values = np.moveaxis(field_values, 0, 2)
+            if not use_autostretch:
+                field_values = image_utils.perform_histogram_clip(field_values, white_point_scale=white_point_scale)
+                was_stretched = True
         else:
             channel_idx = field.channels[filter_label]
             field_values = field_values[channel_idx, :, :]
@@ -311,17 +313,35 @@ def visualize_field(field, only_window=True, approximate_wavelength=None, filter
         wavelength = field.wavelengths[wavelength_idx]
         field_values = field_values[wavelength_idx, :, :]
 
-    if clipping_factor is not None:
-        vmin = np.min(field_values)
-        vmax = np.max(field_values)*clipping_factor
+    if field.dtype == 'complex128' and not use_colors:
+        phases = np.angle(field_values)
+        field_values = np.abs(field_values)
+        phases[field_values == 0] = np.nan
+    else:
+        field_values = field_values.astype('float64')
 
-    log_label = 'log10 ' if use_log else ''
+    if use_log:
+        field_values = np.log10(field_values)
+        log_label = 'log10 '
+    else:
+        log_label = ''
+
+    if not was_stretched:
+        if use_autostretch:
+            field_values = image_utils.perform_autostretch(field_values)
+            vmin = 0
+            vmax = 1
+        else:
+            vmin = np.min(field_values)
+            vmax = np.max(field_values)*white_point_scale
 
     if wavelength is None:
         wavelength = 1
     else:
         wavelength_text = '{:g} nm'.format(wavelength*1e9)
         title = wavelength_text if title == '' else '{} ({})'.format(title, wavelength_text)
+
+    colorbar = not (use_colors or use_autostretch)
 
     xlabel = r'$x$'
     ylabel = r'$y$'
@@ -344,22 +364,13 @@ def visualize_field(field, only_window=True, approximate_wavelength=None, filter
     fig = plt.figure()
 
     if field.dtype == 'complex128' and not use_colors:
-
         left_ax = fig.add_subplot(121)
-        amplitudes = np.abs(field_values)
-        amplitudes = np.log10(amplitudes) if use_log else amplitudes
-        plot_utils.plot_image(fig, left_ax, amplitudes, xlabel=xlabel, ylabel=ylabel, title=title, extent=extent, clabel=clabel)
-
+        plot_utils.plot_image(fig, left_ax, field_values, xlabel=xlabel, ylabel=ylabel, title=title, extent=extent, clabel=clabel)
         right_ax = fig.add_subplot(122)
-        phases = np.angle(field_values)
-        phases[amplitudes == 0] = np.nan
         plot_utils.plot_image(fig, right_ax, phases, xlabel=xlabel, ylabel=ylabel, title=title, extent=extent, clabel=phase_clabel)
-
     else:
         ax = fig.add_subplot(111)
-        field_values = field_values.astype('float64')
-        field_values = np.log10(field_values) if use_log else field_values
-        plot_utils.plot_image(fig, ax, field_values, vmin=vmin, vmax=vmax, xlabel=xlabel, ylabel=ylabel, title=title, extent=extent, clabel=clabel, colorbar=(not use_colors))
+        plot_utils.plot_image(fig, ax, field_values, vmin=vmin, vmax=vmax, xlabel=xlabel, ylabel=ylabel, title=title, extent=extent, clabel=clabel, colorbar=colorbar)
 
     plt.tight_layout()
 
