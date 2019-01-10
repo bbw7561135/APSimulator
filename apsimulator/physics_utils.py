e29f7905f15a3cdefbe71ad12f32f1e3d01941a0
Lars Frogner <lars_frogner@hotmail.com>
2019-01-10 23:42:44 +0100

    Add utility functions for magnitudes and fluxes

--- apsimulator/physics_utils.py
+++ apsimulator/physics_utils.py
@@ -2,6 +2,7 @@
 # This file is part of the APSimulator API.
 # Author: Lars Frogner
 import numpy as np
+import math_utils
 
 
 planck_constant = 6.626070150e-34 # [J s]
@@ -13,13 +14,40 @@ light_year = 9.4607304725808e15 # [m]
 solar_radius = 6.957e8 # [m]
 solar_luminosity = 3.828e26 # [W]
 
-
-def meters_from_parsecs(parsecs):
-    return 3.0857e16*parsecs
+V_band_reference_flux = 3e-9 # [W/m^2]
 
 
-def inverse_cubic_meters_from_inverse_cubic_parsecs(per_cubic_parsec):
-    return 3.404e-50*per_cubic_parsec
+def compute_photons_per_energy_unit(wavelengths):
+    photons_per_energy_unit = wavelengths/(planck_constant*speed_of_light)
+    return photons_per_energy_unit
+
+
+def magnitude_from_flux(flux, reference_flux):
+    return -2.5*np.log10(flux/reference_flux)
+
+
+def flux_from_magnitude(magnitude, reference_flux):
+    return reference_flux*10**(-0.4*magnitude)
+
+
+def V_band_magnitude_from_flux(flux):
+    return magnitude_from_flux(flux, V_band_reference_flux)
+
+
+def V_band_flux_from_magnitude(magnitude):
+    return flux_from_magnitude(magnitude, V_band_reference_flux)
+
+
+def V_band_magnitude_from_bortle_class(bortle_class, field_of_view_solid_angle):
+     bortle_classes = [0, 1, 2, 3, 4, 5, 7]
+     bortle_magnitudes_per_square_arcsec = np.array([22.0, 21.7, 21.5, 21.3, 20.4, 19.1, 18.0])
+     bortle_magnitudes = bortle_magnitudes_per_square_arcsec - 2.5*np.log10(math_utils.square_arcsec_from_steradians(field_of_view_solid_angle))
+     return math_utils.interp_extrap(bortle_class, bortle_classes, bortle_magnitudes)
+
+
+def compute_blackbody_spectral_fluxes(wavelengths, temperature):
+    return (2*np.pi*planck_constant*speed_of_light**2)/((np.exp((planck_constant*speed_of_light/boltzmann_constant)/(wavelengths*temperature)) - 1)
+                                                         *wavelengths**5)
 
 
 class BlackbodyStars:
