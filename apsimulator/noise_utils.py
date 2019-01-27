cfc73680d18ef59c51b29a93a9a36858c9434ee8
Lars Frogner <lars_frogner@hotmail.com>
2019-01-27 15:50:36 +0100

    Use new approach for noise pattern generation

--- apsimulator/noise_utils.py
+++ apsimulator/noise_utils.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 # This file is part of the APSimulator API.
 # Author: Lars Frogner
+import numpy as np
 import os
 import parallel_utils
 import procedural_noise
+import math_utils
 
 
-class ProceduralNoisePattern:
+class FractalNoisePattern:
 
     def __init__(self, number_of_octaves=1,
-                       initial_frequency=0,
-                       frequency_scale=1,
-                       spectral_index=1,
-                       exponential_factor=1,
-                       invert_octaves=False,
+                       initial_frequency=1,
+                       persistence=1,
                        seed=None):
 
         self.set_number_of_octaves(number_of_octaves)
         self.set_initial_frequency(initial_frequency)
-        self.set_frequency_scale(frequency_scale)
-        self.set_spectral_index(spectral_index)
-        self.set_exponential_factor(exponential_factor)
-        self.set_invert_octaves(invert_octaves)
+        self.set_persistence(persistence)
         self.set_seed(seed)
 
     def set_number_of_octaves(self, number_of_octaves):
@@ -32,47 +28,62 @@ class ProceduralNoisePattern:
         self.initial_frequency = float(initial_frequency)
         assert self.initial_frequency >= 0
 
-    def set_frequency_scale(self, frequency_scale):
-        self.frequency_scale = float(frequency_scale)
-        assert self.frequency_scale > 0
-
-    def set_spectral_index(self, spectral_index):
-        self.spectral_index = float(spectral_index)
-        assert self.spectral_index > 0
-
-    def set_exponential_factor(self, exponential_factor):
-        self.exponential_factor = float(exponential_factor)
-        assert self.exponential_factor != 0
-
-    def set_invert_octaves(self, invert_octaves):
-        self.invert_octaves = bool(invert_octaves)
+    def set_persistence(self, persistence):
+        self.persistence = float(persistence)
+        assert self.persistence > 0
 
     def set_seed(self, seed):
         if seed is None:
             # Get random integer from operating system
-            self.seed = int.from_bytes(os.urandom(4), byteorder='big')
+            self.seed = int.from_bytes(os.urandom(2), byteorder='big')
         else:
             self.seed = int(seed)
 
-    def compute(self, x_coordinates, y_coordinates, z_coordinates):
+    def compute(self, x_coordinates, y_coordinates, z_coordinates, smallest_scale=None, largest_scale=None, mask=None):
 
         assert isinstance(x_coordinates, np.ndarray)
         assert isinstance(y_coordinates, np.ndarray)
         assert isinstance(z_coordinates, np.ndarray)
+        assert x_coordinates.dtype == np.dtype('float32')
+        assert y_coordinates.dtype == np.dtype('float32')
+        assert z_coordinates.dtype == np.dtype('float32')
 
-        pattern_values = np.empty((z_coordinates.size, y_coordinates.size, x_coordinates.size), dtype='float64')
+        if x_coordinates.ndim == 1 and y_coordinates.ndim == 1 and z_coordinates.ndim == 1:
+            x_coordinates, y_coordinates, z_coordinates = np.meshgrid(x_coordinates, y_coordinates, z_coordinates, indexing='ij')
+        else:
+            assert x_coordinates.ndim == 3 and y_coordinates.ndim == 3 and z_coordinates.ndim == 3
+            assert x_coordinates.shape == y_coordinates.shape
+            assert x_coordinates.shape == z_coordinates.shape
 
-        procedural_noise.generate_noise_pattern(pattern_values,
-                                                x_coordinates.astype('float64'),
-                                                y_coordinates.astype('float64'),
-                                                z_coordinates.astype('float64'),
-                                                self.number_of_octaves,
+        noise_values = np.zeros(x_coordinates.shape, dtype='float32')
+
+        if mask is None:
+            mask = np.ones(noise_values.shape, dtype='uint8')
+        else:
+            assert mask.shape == noise_values.shape
+            mask = mask.astype('uint8')
+
+        if largest_scale is not None and smallest_scale is not None:
+            # Limit the number of octaves to avoid wasteful noise generation at subgrid scales
+            number_of_octaves = min(self.number_of_octaves, self.compute_max_number_of_octaves(smallest_scale, largest_scale))
+        else:
+            number_of_octaves = self.number_of_octaves
+
+        procedural_noise.generate_fractal_noise(noise_values,
+                                                x_coordinates,
+                                                y_coordinates,
+                                                z_coordinates,
+                                                mask,
+                                                number_of_octaves,
                                                 self.initial_frequency,
-                                                self.frequency_scale,
-                                                self.spectral_index,
-                                                self.exponential_factor,
-                                                int(self.invert_octaves),
+                                                self.persistence,
                                                 self.seed,
                                                 parallel_utils.get_number_of_threads())
 
-        return pattern_values
+        return noise_values
+
+    def compute_smallest_noise_extent(self, largest_grid_extent):
+        return 1/(largest_grid_extent*self.initial_frequency*2**(self.number_of_octaves - 1))
+
+    def compute_max_number_of_octaves(self, smallest_grid_cell_extent, largest_grid_extent):
+        return 1 + math_utils.nearest_lower_power_of_2_exponent(1/(smallest_grid_cell_extent*largest_grid_extent*self.initial_frequency))
--- apsimulator/procedural_noise.c
+++ apsimulator/procedural_noise.c
@@ -6,7 +6,7 @@
 #include "open-simplex-noise.h"
 
 // Function declarations
-static PyObject* generate_noise_pattern(PyObject*, PyObject*);
+static PyObject* generate_fractal_noise(PyObject*, PyObject*);
 
 // Method definition object for this extension, these argumens mean:
 // ml_name: The name of the method
@@ -16,7 +16,7 @@ static PyObject* generate_noise_pattern(PyObject*, PyObject*);
 //          class method, or being a static method of a class.
 // ml_doc:  Contents of this method's docstring
 static PyMethodDef procedural_noise_methods[] = {
-    { "generate_noise_pattern", generate_noise_pattern, METH_VARARGS, NULL},
+    { "generate_fractal_noise", generate_fractal_noise, METH_VARARGS, NULL},
     {NULL, NULL, 0, NULL}
 };
 
@@ -41,129 +41,136 @@ PyMODINIT_FUNC PyInit_procedural_noise(void) {
     return PyModule_Create(&procedural_noise_definition);
 }
 
-static PyObject* generate_noise_pattern(PyObject* self, PyObject* args)
+static PyObject* generate_fractal_noise(PyObject* self, PyObject* args)
 {
     /*
-    Generates coherent noise values for the 3D mesh defined by the given 1D coordinate
+    Generates coherent fractal noise for the 3D mesh defined by the given 3D coordinate
     arrays and inserts them into the give 3D pattern_values array.
-
     Usage:
-
-    generate_noise_pattern(pattern_values,
+    generate_fractal_noise(noise_values,
                            x_coordinates, y_coordinates, z_coordinates,
+                           mask,
                            number_of_octaves,
                            initial_frequency,
-                           frequency_scale,
-                           spectral_index,
-                           exponential_factor,
-                           invert_octaves,
+                           persistence,
                            seed,
                            n_threads)
-
-    The noise value for point (x, y, z) is computed as
-
-    Phi(x, y, z, omega, k0, fs, nu, alpha) = (sum_{k=1,..,omega} Theta((k0 + fs*k)**(-nu)*P((k0 + fs*k)*(x, y, z))))**alpha
-
-    where omega is the number of octaves (frequencies), k0 is the initial frequency,
-    fs is the frequency scale, nu is the spectral index, alpha is an exponential
-    factor, Theta(x) = x or 1/z and P(x, y, z) is the simplex noise function.
     */
 
     // Input arguments
-    PyArrayObject* pattern_value_array;
+    PyArrayObject* noise_value_array;
     PyArrayObject* x_coordinate_array;
     PyArrayObject* y_coordinate_array;
     PyArrayObject* z_coordinate_array;
+    PyArrayObject* mask_array;
     int number_of_octaves;
-    double initial_frequency;
-    double frequency_scale;
-    double spectral_index;
-    double exponential_factor;
-    int invert_octaves;
+    float initial_frequency;
+    float persistence;
     int seed;
     int n_threads;
 
-    double* pattern_values = NULL;
-    double* x_coordinates = NULL;
-    double* y_coordinates = NULL;
-    double* z_coordinates = NULL;
+    float* noise_values = NULL;
+    float* x_coordinates = NULL;
+    float* y_coordinates = NULL;
+    float* z_coordinates = NULL;
+    unsigned char* mask = NULL;
 
     int size_x, size_y, size_z;
     int i, j, k, f;
-    int z_offset, zy_offset;
-    double scaled_frequency;
-    double octave_value;
-    double pattern_value;
+    int x_offset, xy_offset;
+    int idx;
+    float noise_value;
+    float total_amplitude;
+    float frequency;
+    float amplitude;
 
     struct osn_context* context;
 
     // Get argument data
-    if (!PyArg_ParseTuple(args, "O!O!O!O!iddddiii",
-                          &PyArray_Type, &pattern_value_array,
+    if (!PyArg_ParseTuple(args, "O!O!O!O!O!iffii",
+                          &PyArray_Type, &noise_value_array,
                           &PyArray_Type, &x_coordinate_array,
                           &PyArray_Type, &y_coordinate_array,
                           &PyArray_Type, &z_coordinate_array,
+                          &PyArray_Type, &mask_array,
                           &number_of_octaves,
                           &initial_frequency,
-                          &frequency_scale,
-                          &spectral_index,
-                          &exponential_factor,
-                          &invert_octaves,
+                          &persistence,
                           &seed,
                           &n_threads))
     {
         return NULL;
     }
 
-    size_x = x_coordinate_array->dimensions[0];
-    size_y = y_coordinate_array->dimensions[0];
-    size_z = z_coordinate_array->dimensions[0];
+    size_x = noise_value_array->dimensions[0];
+    size_y = noise_value_array->dimensions[1];
+    size_z = noise_value_array->dimensions[2];
 
-    assert(pattern_value_array->dimensions[0] == size_z);
-    assert(pattern_value_array->dimensions[1] == size_y);
-    assert(pattern_value_array->dimensions[2] == size_x);
+    assert(x_coordinate_array->dimensions[0] == size_x);
+    assert(x_coordinate_array->dimensions[1] == size_y);
+    assert(x_coordinate_array->dimensions[2] == size_z);
+
+    assert(y_coordinate_array->dimensions[0] == size_x);
+    assert(y_coordinate_array->dimensions[1] == size_y);
+    assert(y_coordinate_array->dimensions[2] == size_z);
+
+    assert(z_coordinate_array->dimensions[0] == size_x);
+    assert(z_coordinate_array->dimensions[1] == size_y);
+    assert(z_coordinate_array->dimensions[2] == size_z);
+
+    assert(mask_array->dimensions[0] == size_x);
+    assert(mask_array->dimensions[1] == size_y);
+    assert(mask_array->dimensions[2] == size_z);
 
     assert(number_of_octaves > 0);
-    assert(initial_frequency >= 0);
-    assert(frequency_scale > 0);
-    assert(spectral_index > 0);
-    assert(exponential_factor != 0);
-    assert(invert_octaves == 0 || invert_octaves == 1);
+    assert(initial_frequency > 0);
+    assert(persistence > 0);
     assert(seed >= 0);
     assert(n_threads > 0);
 
-    pattern_values = (double*)pattern_value_array->data;
-    x_coordinates = (double*)x_coordinate_array->data;
-    y_coordinates = (double*)y_coordinate_array->data;
-    z_coordinates = (double*)z_coordinate_array->data;
+    noise_values = (float*)noise_value_array->data;
+    x_coordinates = (float*)x_coordinate_array->data;
+    y_coordinates = (float*)y_coordinate_array->data;
+    z_coordinates = (float*)z_coordinate_array->data;
+    mask = (unsigned char*)mask_array->data;
 
     open_simplex_noise(seed, &context);
 
-    #pragma omp parallel for num_threads(n_threads) schedule(static) default(shared) private(i, j, k, f, z_offset, zy_offset, pattern_value, scaled_frequency, octave_value)
-    for (k = 0; k < size_z; k++)
+    #pragma omp parallel for num_threads(n_threads) schedule(static) default(shared) private(i, j, k, f, x_offset, xy_offset, idx, noise_value, total_amplitude, frequency, amplitude)
+    for (i = 0; i < size_x; i++)
     {
-        z_offset = k*size_y;
+        x_offset = i*size_y;
 
         for (j = 0; j < size_y; j++)
         {
-            zy_offset = (z_offset + j)*size_x;
+            xy_offset = (x_offset + j)*size_z;
 
-            for (i = 0; i < size_x; i++)
+            for (k = 0; k < size_z; k++)
             {
-                pattern_value = 0;
+                idx = xy_offset + k;
 
-                for (f = 1; f < number_of_octaves+1; f++)
+                if (mask[idx] > 0)
                 {
-                    scaled_frequency = initial_frequency + frequency_scale*f;
-                    octave_value = open_simplex_noise3(context, scaled_frequency*x_coordinates[i],
-                                                                scaled_frequency*y_coordinates[j],
-                                                                scaled_frequency*z_coordinates[k]);
-                    octave_value *= pow(1/scaled_frequency, spectral_index); // Could be precomputed
-                    pattern_value += (invert_octaves)? 1/octave_value : octave_value;
-                }
+                    noise_value = 0;
 
-                pattern_value = pow(pattern_value, exponential_factor);
-                pattern_values[zy_offset + i] = pattern_value;
+                    total_amplitude = 0;
+                    frequency = initial_frequency;
+                    amplitude = 1;
+
+                    for (f = 0; f < number_of_octaves; f++)
+                    {
+                        noise_value += (float)open_simplex_noise3(context, frequency*x_coordinates[idx],
+                                                                           frequency*y_coordinates[idx],
+                                                                           frequency*z_coordinates[idx])
+                                       *amplitude;
+
+                        total_amplitude += amplitude;
+                        frequency *= 2;
+                        amplitude *= persistence;
+                    }
+
+                    noise_values[idx] = noise_value/total_amplitude;
+                }
             }
         }
     }
