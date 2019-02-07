9640952d8e6188999282251b9e889b068e814ae3
Lars Frogner <lars_frogner@hotmail.com>
2019-02-08 00:32:49 +0100

    Remove automatic rescaling before gamma correction and add clamp function

--- apsimulator/image_utils.py
+++ apsimulator/image_utils.py
@@ -7,14 +7,20 @@ import numpy as np
 def perform_liear_stretch(image, black_point, white_point):
     '''
     Perform a linear stretch to make the "black_point" pixel values black
-    and the "white_point" pixel values white. The returned image has
-    values in the range [0, 1].
+    and the "white_point" pixel values white.
     '''
     return (image - black_point)/(white_point - black_point)
 
 
 def perform_histogram_clip(image, black_point_scale=1, white_point_scale=1):
     return perform_liear_stretch(image, np.min(image)*black_point_scale, np.max(image)*white_point_scale)
+
+
+def clamp(image, minimum_value, maximum_value):
+    clamped_image = image.copy()
+    clamped_image[image < minimum_value] = minimum_value
+    clamped_image[image > maximum_value] = maximum_value
+    return clamped_image
 
 
 def perform_midtone_stretch(image, midtones_balance):
@@ -43,10 +49,8 @@ def perform_autostretch(image, new_mean=0.25):
 
 
 def perform_fast_gamma_correction(image):
-    clipped_image = perform_histogram_clip(image)
-    return np.sqrt(clipped_image) # gamma = 1/2
+    return np.sqrt(image) # gamma = 1/2
 
 
 def perform_gamma_correction(image, gamma=1/2.2):
-    clipped_image = perform_histogram_clip(image)
-    return perform_fast_gamma_correction(image) if gamma == 0.5 else clipped_image**gamma
+    return image if gamma == 1 else (perform_fast_gamma_correction(image) if gamma == 0.5 else image**gamma)
